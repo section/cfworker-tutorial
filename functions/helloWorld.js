99d7d742822064a286693d9acce96b5963563ef2
Ben Poh <ben.poh@section.io>
2022-12-14 16:19:20 +1100

    Update hello world template
    Signed-off-by: Ben Poh <ben.poh@section.io>

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -3,6 +3,11 @@ const helloWorld = async () => {
 <body>
   <h1>Hello World</h1>
   <p>Hosted on Section. Refer to our <a href="https://www.section.io/docs/tutorials/" target="_blank" rel="noopener noreferrer">docs</a>.</p>
+
+  <ul>
+    <li><a href="/">/</a></li>
+    <li><a href="/section">/section</a></li>
+  </ul>
 </body>`
     return html
 }
